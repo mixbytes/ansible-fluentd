9d56774e6aa0ffb53fa86d1b92dc024fd9d2dbe8
Andrey Levkin <alevkin@gmail.com>
2019-06-06 20:29:12 +0300

    Fix filter plugin and linter wrrors/warnings

--- filter_plugins/fluentd_params.py
+++ filter_plugins/fluentd_params.py
@@ -1,10 +1,12 @@
 from ansible.module_utils._text import to_text
 from collections import OrderedDict
 
+
 def fluentd_params_to_text(params):
     config = ""
     if type(params) == dict:
-        params = OrderedDict(sorted(params.items(), key=lambda x: x[0], reverse=True))
+        params = OrderedDict(sorted(
+            params.items(), key=lambda x: x[0], reverse=True))
     else:
         raise ValueError("Params should be dictionary")
     for key, val in params.items():
@@ -13,19 +15,21 @@ def fluentd_params_to_text(params):
         if type(val) is list:
             if all([type(x) == dict for x in val]):
                 for v in val:
-                    config += "<{0}>\n  {1}</{0}>\n  ".format(key, params_to_text(v))
+                    config += "<{0}>\n  {1}</{0}>\n  ".format(
+                        key, fluentd_params_to_text(v))
             else:
-                config += "{0} {1}\n  ".format(key, val.join(', '))
+                config += "{0} {1}\n  ".format(
+                    key, val.join(', '))
         elif type(val) is dict:
-            config += "<{0}>\n  {1}</{0}>\n  ".format(key, params_to_text(val))
+            config += "<{0}>\n  {1}</{0}>\n  ".format(
+                key, fluentd_params_to_text(val))
         else:
             config += "{0} {1}\n  ".format(key, val)
-            
+ 
     return to_text(config)
 
 
 class FilterModule(object):
 
-
-   def filters(self):
-       return {'fluentd_params_to_text': fluentd_params_to_text } 
+    def filters(self):
+        return {'fluentd_params_to_text': fluentd_params_to_text}
